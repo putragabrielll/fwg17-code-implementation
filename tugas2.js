9e02aef14672d71d7cb002b1d44f57d157e1e315
putragabrielll <putragmahk@gmail.com>
2023-11-03 00:22:22 +0700

    update : tugas 2

--- tugas2.js
+++ tugas2.js
@@ -8,8 +8,8 @@ console.log(kalimat[22]);
 temp = "";
 // temp2 = javascpript belajar saya 
 temp2 = "";
-kalimat = kalimat + " "
 if (typeof kalimat == "string") {
+    // kalimat = kalimat + " "
     for (let i = 0; i < kalimat.length; i++) {
         if (kalimat[i] == " ") {
         temp2 = temp + " " + temp2;
@@ -17,11 +17,21 @@ if (typeof kalimat == "string") {
         } else {
         temp = temp + kalimat[i];
         }
-        // if (i == kalimat.length - 1) {
-        // temp2 = temp + " " + temp2;
-        // }
+        if (i == kalimat.length - 1) {
+        temp2 = temp + " " + temp2;
+        }
     }
 } else {
     return console.log("Masukkan tipe data string");
 }
 console.log(temp2);
+
+
+
+console.log(typeof ('' + 113))
+console.log('' + 113)
+'113'
+
+console.log(typeof (113 - ''))
+console.log(113 - '')
+113
