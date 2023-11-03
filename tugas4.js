99854239daeb43be4713e4cc306961fee455dbb5
putragabrielll <putragmahk@gmail.com>
2023-11-03 15:43:29 +0700

    update : tugas 4

--- tugas4.js
+++ tugas4.js
@@ -1,5 +1,5 @@
 const urutan = (x) => {
-    let perbaikanData = x.toString().split('0') // merubah ke string terlebih dahulu lalu di split)
+    let perbaikanData = x.toString().split('0') // merubah ke string terlebih dahulu lalu di split()
     let temp = ''
     for (let i = 0; i < perbaikanData.length; i++){
         let hasil = perbaikanData[i].split('').sort().join('') // perbaikanData dengan index ke i akan di split() lagi lalu di sort() dan setelah di urutkan maka akan di gabungkan kembali dengan method join()
