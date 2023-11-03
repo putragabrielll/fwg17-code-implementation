275b6d753e35582b64ac14ecab2c5eda056e1a75
putragabrielll <putragmahk@gmail.com>
2023-11-03 11:34:52 +0700

    add : tugas 4

---
+++ tugas4.js
@@ -0,0 +1,10 @@
+const urutan = (x) => {
+    let perbaikanData = x.toString().split('0') // merubah ke string terlebih dahulu lalu di split)
+    let temp = ''
+    for (let i = 0; i < perbaikanData.length; i++){
+        let hasil = perbaikanData[i].split('').sort().join('') // perbaikanData dengan index ke i akan di split() lagi lalu di sort() dan setelah di urutkan maka akan di gabungkan kembali dengan method join()
+        temp += hasil // akan menambakan ke variable penampung temp
+    }
+    return temp
+}
+console.log(urutan(5956560159466056))
